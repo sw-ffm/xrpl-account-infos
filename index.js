94c80a8c8e0e1851006c994366f15d4be7933199
Stefan Watzinger <stefanwatzingerfrankfurt@gmail.com>
2024-07-10 13:49:40 +0200

    updates

--- index.js
+++ index.js
@@ -1,62 +1,57 @@
 require('dotenv').config();
 
 const xrpl = require("xrpl");
+
 const fs = require('node:fs/promises');
-const client = new xrpl.Client( process.env.PUBLIC_SERVER );
 
+const xrpl_client = {
 
-var accounts = process.env.XRPL_ACCOUNTS.split(',');
-var i=0;
+    client: new xrpl.Client( process.env.PUBLIC_SERVER ),
+    accounts: process.env.XRPL_ACCOUNTS.split(','),
 
+    async accountInfo(){
 
-async function accountInfo( client, account ){
-
-    const response = await client.request({
-
-        "command": "account_info",
-        "account": account,
-        "ledger_index": "validated"
-    
-    });
-
-    fs.writeFile(`./json/${account}.json`, JSON.stringify(response))
-    .then(()=>{
-
-        console.log(`${account}.json file written`);
-
-    });    
-
-};
-
-async function closeConnection(){
-
-    await client.disconnect();
-
-};
-
-client
-.connect()
-.then(()=>{
-
-    accounts.forEach(element => {
+        for( const element of this.accounts ){
         
-        accountInfo( client, element )
-        .then(() => {
-
-            i++;
+            const response = await this.client.request({
+        
+                "command": "account_info",
+                "account": element,
+                "ledger_index": "validated"
             
-            if(i >= accounts.length){
+            });
+        
+            await fs.writeFile(`./json/${element}.json`, JSON.stringify(response));
 
-                closeConnection()
-                .then(() => {
+            console.log(`${element}.json file written`);
 
-                    console.log("Job done");
+        }
+    
+    },
 
-                });
+    async closeConnection(){
 
-            }
+        await this.client.disconnect();
+    
+    },
 
-        })
+    async openConnection(){
+
+        await this.client.connect();
+    
+    },
+
+};
+
+xrpl_client.openConnection().then(()=>{
+
+    xrpl_client.accountInfo().then(()=>{
+
+        xrpl_client.closeConnection().then(()=>{
+
+            console.log("Connection closed");
+
+        });
 
     });
 
